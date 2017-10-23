487e1d40749bb262b30c8c1857890373728a4456
IgorMatsepura <Igor.Matsepura@gmail.com>
2017-10-23 13:13:15 +0300

    homeWrk4 v4

--- main.c
+++ main.c
@@ -1,150 +1,155 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-const int pinAdmin=1000;
-const int pinUser=1002;
-const int user=5000;
+
+const int PINADMIN = 1000;  // Static variable PIN - administrator
+const int PINUSER = 1002;   // Test static variable PIN - user
+const int USER = 5000;      // Variable number of users and  Pin-codes
+enum { MIN_PINCODE = 1000, MAX_PINCODE = 9999 };  // Variable range of Pin-code input is not less than 1000 to 9999
+const int RETRYPIN = 2;     // Max input  bad pin-code
+const int SUMARRAY = 125;   // Static sum for array sum
 
 int main()
 {
-    int scannum, pinKod, let, userInput, ch, enterNumb;
-    int CashInput, pinErr;
+    int enterNumb;
+    int pinCode;
+    int enterNumbers, userInput, adminInput;
+    int cashInput;
     int i,j,z,k;
-    int n = 2;
-    int arr[user];
-    int arr1[user];
-    pinKod = 0;
-    scannum = 0;
-    char str[2];
-    enum { min_val = 1000, max_val = 9999 };
-    srand(time(NULL));
-        //generate Random Pin  entry by array data
-        for ( i = 0; i < user; i ++){
-        arr1[i]  = user + i*125;
+    int arrayPinCod[USER];
+    int arraySum[USER];
+    pinCode = 0;
+    enterNumb = 0;
+    enterNumbers = 0;
+
+        //generate Summ and Pin  entry by array data
+        for ( i = 0; i < USER; i ++){
+            arraySum[i]  = USER + i * SUMARRAY;
         }
 
-        for ( j = 0; j < user; j ++){
-
-            if  (rand()%10000 > 10000) {
-            10000 - rand()%10000 ;
-
-
-            if (rand()%10000 < 1000) {
-            1000 + rand()%10000 ;
-            }
-
-            }
-            arr[j]  = 1000 + rand()% 10000;
+        for ( j = 0; j < USER; j ++){
+            arrayPinCod[j]  = PINUSER + j;
+            printf("%d\t", arrayPinCod[j]);
         }
         // Entering a loop with the condition that 3 attempts to enter a pin
-        for ( k = 0 ; k <= 2; k++){
+        for ( k = 0; k <= RETRYPIN; k++){
             for (;;){
-            printf("\n Enter Pin : ");
-            let = scanf("%4d""%1[^\n]""%*[^\n]",&scannum,str);
+                printf("\n Enter Pin : ");
+                enterNumbers = scanf("%d",&enterNumb);
+
             // Checking the Input Values
-            if (scannum < 0){
+            if (enterNumb < 0){
                 printf("\n It's negative Numbers\n");
                 continue;
             }
-
-            if (let != 1) {
-            printf("\n It's letter input number... \n");
-            fflush(stdin);
-            continue;
+            // Checking the Input Letters
+            if (enterNumbers == 0) {
+                printf("\n It's letter input number... \n");
+                fflush(stdin);
+                continue;
             }
-
-            else if (scannum < min_val || scannum > max_val) {
-            printf("Number must be in range %d..%d\n", min_val, max_val);
+            // Checking  Values range of Pin-code
+            if (enterNumb < MIN_PINCODE || enterNumb > MAX_PINCODE) {
+                printf("Number must be in range %d..%d\n", MIN_PINCODE, MAX_PINCODE);
+                continue;
             }
         break;
         }
-        // Pin match search and go to the user menu
-        for ( j = 0; j < user; j ++){
-            if (scannum ==  arr[j]){
-            pinKod = arr[j];
-            goto userSelect;
-            }
+
+        if (enterNumb == !pinCode){
+            printf("Pin-code is not correct...");
         }
 
         // If 3 attempts - programm blocked Card
-        if (k == n) {
-        printf("The Card is reject. Please call in your bank\n");
-        break;
+        if (k == RETRYPIN) {
+            printf("The Card is reject. Please call in your bank\n");
+            break;
+        }
+        // Pin match search and go to the user menu
+        for ( j = 0; j < USER; j ++){
+            if (enterNumb ==  arrayPinCod[j]){
+                pinCode = arrayPinCod[j];
+                break;
+            }
         }
         // Main User Select
-        if (scannum == pinUser || scannum == pinKod  ){
-            userSelect:
-            printf("\nUser detected....\n");
-            printf("1. Account balance.... \n");
-            printf("2. Cash money.... \n");
-            printf("3. Fund your balance...\n");
-            printf("4. Exit take Card....\n");
-            printf("\nOperation selected: ");
-            scanf("%d",&userInput);
-            if ((userInput < 0) || (ch > 4)){
-                printf("\nSelect - incorrect. Correct select 1,2,3,4.\n");
-                continue;
-            }
-            switch (userInput){
-            case 1:
-                printf("Balance %d\n\n", arr1[j]);
-                goto userSelect;
-            case 2:
-                printf("Enter sum: ");
-                scanf("%d",&CashInput);
-                printf("Cash - %d\n", CashInput);
-                CashInput = arr1[j] - CashInput;
-                arr1[j] = CashInput;
-                printf("Operation successful...\n");
-                CashInput = 0;
-                goto userSelect;
-            case 3:
-                printf("Enter sum: ");
-                scanf("%d",&CashInput);
-                printf("\nCash + %d\n", CashInput);
-                CashInput = arr1[j] + CashInput;
-                arr1[j] = CashInput;
-                printf("Operation successful...\n");
-                CashInput = 0;
-                goto userSelect;
-            case 4:
-                break;
-                fflush(stdin);
+        if (enterNumb == pinCode){
+            for (;;){
+                while (userInput != 4){
+                    printf("\nUser detected....\n");
+                    printf("1. Account balance.... \n");
+                    printf("2. Cash money.... \n");
+                    printf("3. Fund your balance...\n");
+                    printf("4. Exit take Card....\n");
+                    printf("\nOperation selected: ");
+                    scanf("%d",&userInput);
+                    if ((userInput < 0) || (userInput > 4)){
+                        printf("\nSelect - incorrect. Correct select 1,2,3,4.\n");
+                    }
+                    switch (userInput){
+                        case 1:
+                            printf("Balance %d\n", arraySum[j]);
+                            break;
+                        case 2:
+                            printf("Enter sum: ");
+                            scanf("%d",&cashInput);
+                            printf("Cash - %d\n", cashInput);
+                            cashInput = arraySum[j] - cashInput;
+                            arraySum[j] = cashInput;
+                            printf("Operation successful...\n");
+                            cashInput = 0;
+                            break;
+                        case 3:
+                            printf("Enter sum: ");
+                            scanf("%d",&cashInput);
+                            printf("\nCash + %d\n", cashInput);
+                            cashInput = arraySum[j] + cashInput;
+                            arraySum[j] = cashInput;
+                            printf("Operation successful...\n");
+                            cashInput = 0;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            break;
             }
         k = 0;
         userInput = 0;
         }
         // Main admin Select
-        if (scannum == pinAdmin){
-            while (ch != 3){
-            printf("Supervisor main Select....\n");
-            printf("1. View all data.... \n");
-            printf("2. Clear all data.... \n");
-            printf("3. Exit main Supervisor....\n");
-            printf("Operation selected: ");
-            scanf("%d", &ch);
-                if ((ch < 0) || (ch > 3)){
-                printf("\nSelect - incorrect. Correct select 1,2,3.\n");
-                continue;
+        if (enterNumb == PINADMIN){
+            while (adminInput != 3){
+                printf("Supervisor main Select....\n");
+                printf("1. View all data.... \n");
+                printf("2. Clear all data.... \n");
+                printf("3. Exit main Supervisor....\n");
+                printf("Operation selected: ");
+                scanf("%d", &adminInput);
+                if ((adminInput < 0) || (adminInput > 3)){
+                    printf("\nSelect - incorrect. Correct select 1,2,3.\n");
+                    continue;
             }
-            switch (ch){
+            switch (adminInput){
             case 1:
-                for ( i = 0, j = 0; i < user; i ++, j++ ){
-                printf("Summ - %d Pin - %d\n",  arr1[i], arr[j]);
+                for ( i = 0, j = 0; i < USER; i ++, j++ ){
+                    printf("Summ - %d Pin - %d\n",  arraySum[i], arrayPinCod[j]);
                 }
                 break;
             case 2:
-                printf("Clear all array...");
-                memset(arr1, 0, sizeof(arr1));
-                memset(arr, 0, sizeof(arr));
+                printf("Clear all posts Sum and Pin...");
+                memset(arraySum, 0, sizeof(arraySum));
+                memset(arrayPinCod, 0, sizeof(arrayPinCod));
                 printf("Successful...\n");
                 break;
-            //case 3:
+            default:
+                break;
             }
             }
-        k = 0;
-        ch = 0;
+            k = 0;
+            enterNumb = 0;
+            adminInput = 0;
+            fflush(stdin);
         }
     }
     return 0;
